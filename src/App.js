a797fae8afe0c832360c21588875c360030e187d
minji <lee.minji9521@gmail.com>
2024-07-01 13:58:10 -0400

    redirect when not authenticate

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import AboutPage from './page/AboutPage';
 import MoreAboutPage from './page/MoreAboutPage';
 import ProductPage from './page/ProductPage';
 import ProductDetailPage from './page/ProductDetailPage';
-import { Route, Routes } from 'react-router-dom';
+import LoginPage from './page/LoginPage';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import UserPage from './page/UserPage';
 
 function App() {
+  const [ authenticate, setAuthenticate ] = useState(false)
+
+  const PrivateRoute = () => { // component as it's camel case
+    return authenticate == true? <UserPage />: <Navigate to='/login'/>
+  }
+
   return (
     <div className="App">
       <Routes>
@@ -15,6 +24,8 @@ function App() {
         <Route path='/products/:id' element={<ProductDetailPage />} />
         <Route path='/about' element={<AboutPage />} />
         <Route path='/about/:id' element={< MoreAboutPage/>} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/user' element={<PrivateRoute />} />
       </Routes>
 
     </div>
